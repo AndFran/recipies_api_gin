505e660dab1ab21ffb9588f1dafe7805ad9c5c2b
AndFran <andrewwilliamfranks@gmail.com>
2024-09-08 14:26:34 +0200

    connection mongo db

--- main.go
+++ main.go
@@ -1,272 +1,21 @@
-// Recipes API
-//
-// This is a sample recipes API. You can find out more about the API at https://github.com/PacktPublishing/Building-Distributed-Applications-in-Gin.
-//
-//		Schemes: http
-//	 Host: localhost:8080
-//		BasePath: /
-//		Version: 1.0.0
-//		Contact: Mohamed Labouardy <a@a.com>
-//
-//		Consumes:
-//		- application/json
-//
-//		Produces:
-//		- application/json
-//
-// swagger:meta
 package main
 
 import (
 	"context"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
-	"net/http"
 	"os"
-	"strings"
-	"time"
-
-	"github.com/gin-gonic/gin"
+	"recipies_api_gin/handlers"
 )
 
-var recipes []Recipe
-
-// swagger:parameters recipes newRecipe
-type Recipe struct {
-	//swagger:ignore
-	ID           primitive.ObjectID `json:"id" bson:"_id"`
-	Name         string             `json:"name" bson:"name"`
-	Tags         []string           `json:"tags" bson:"tags"`
-	Ingredients  []string           `json:"ingredients" bson:"ingredients"`
-	Instructions []string           `json:"instructions" bson:"instructions"`
-	PublishedAt  time.Time          `json:"publishedAt" bson:"publishedAt"`
-}
-
-// swagger:operation GET /recipes recipes listRecipes
-// Returns list of recipes
-// ---
-// produces:
-// - application/json
-// responses:
-//
-//	'200':
-//	    description: Successful operation
-func ListRecipesHandler(c *gin.Context) {
-	ctx := context.Background()
-	collection := client.Database("demo").Collection("recipes")
-	cur, err := collection.Find(ctx, bson.M{})
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	defer cur.Close(ctx)
-
-	r := make([]Recipe, 0)
-	for cur.Next(ctx) {
-		var rec Recipe
-		cur.Decode(&rec)
-		r = append(r, rec)
-	}
-	c.JSON(http.StatusOK, r)
-}
-
-// swagger:operation POST /recipes recipes newRecipe
-// Create a new recipe
-// ---
-// produces:
-// - application/json
-// responses:
-//
-//	'200':
-//	    description: Successful operation
-//	'400':
-//	    description: Invalid input
-func NewRecipeHandler(c *gin.Context) {
-	var recipe Recipe
-	if err := c.ShouldBindJSON(&recipe); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	recipe.ID = primitive.NewObjectID()
-	recipe.PublishedAt = time.Now()
-
-	collection := client.Database("demo").Collection("recipes")
-	_, err = collection.InsertOne(context.Background(), recipe)
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusCreated, recipe)
-}
-
-// swagger:operation PUT /recipes/{id} recipes updateRecipe
-// Update an existing recipe
-// ---
-// parameters:
-//   - name: id
-//     in: path
-//     description: ID of the recipe
-//     required: true
-//     type: string
-//
-// produces:
-// - application/json
-// responses:
-//
-//	'200':
-//	    description: Successful operation
-//	'400':
-//	    description: Invalid input
-//	'404':
-//	    description: Invalid recipe ID
-func UpdateRecipeHandler(c *gin.Context) {
-	id := c.Param("id")
-	var recipe Recipe
-	if err := c.ShouldBindJSON(&recipe); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	objectId, _ := primitive.ObjectIDFromHex(id)
-	collection := client.Database("demo").Collection("recipes")
-	_, err = collection.UpdateOne(context.Background(), bson.M{"_id": objectId}, bson.D{
-		{"$set", bson.D{
-			{"name", recipe.Name},
-			{"instructions", recipe.Instructions},
-			{"ingredients", recipe.Ingredients},
-			{"tags", recipe.Tags},
-		}}})
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"message": "Recipe updated"})
-}
-
-// swagger:operation DELETE /recipes/{id} recipes deleteRecipe
-// Delete an existing recipe
-// ---
-// produces:
-// - application/json
-// parameters:
-//   - name: id
-//     in: path
-//     description: ID of the recipe
-//     required: true
-//     type: string
-//
-// responses:
-//
-//	'200':
-//	    description: Successful operation
-//	'404':
-//	    description: Invalid recipe ID
-func DeleteRecipeHandler(c *gin.Context) {
-	id := c.Param("id")
-	log.Println(id)
-	/*index := -1
-	for i := 0; i < len(recipes); i++ {
-		if recipes[i].ID == id {
-			index = i
-			break
-		}
-	}
-
-	if index == -1 {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Recipe not found"})
-		return
-	}
-
-	recipes = append(recipes[:index], recipes[index+1:]...)*/
-
-	c.JSON(http.StatusOK, gin.H{"message": "Recipe has been deleted"})
-}
-
-// swagger:operation GET /recipes/search recipes findRecipe
-// Search recipes based on tags
-// ---
-// produces:
-// - application/json
-// parameters:
-//   - name: tag
-//     in: query
-//     description: recipe tag
-//     required: true
-//     type: string
-//
-// responses:
-//
-//	'200':
-//	    description: Successful operation
-func SearchRecipesHandler(c *gin.Context) {
-	tag := c.Query("tag")
-	listOfRecipes := make([]Recipe, 0)
-
-	for i := 0; i < len(recipes); i++ {
-		found := false
-		for _, t := range recipes[i].Tags {
-			if strings.EqualFold(t, tag) {
-				found = true
-			}
-		}
-		if found {
-			listOfRecipes = append(listOfRecipes, recipes[i])
-		}
-	}
-
-	c.JSON(http.StatusOK, listOfRecipes)
-}
-
-// swagger:operation GET /recipes/{id} recipes oneRecipe
-// Get one recipe
-// ---
-// produces:
-// - application/json
-// parameters:
-//   - name: id
-//     in: path
-//     description: ID of the recipe
-//     required: true
-//     type: string
-//
-// responses:
-//
-//	'200':
-//	    description: Successful operation
-//	'404':
-//	    description: Invalid recipe ID
-func GetRecipeHandler(c *gin.Context) {
-	id := c.Param("id")
-	log.Println(id)
-	/*for i := 0; i < len(recipes); i++ {
-		if recipes[i].ID == id {
-			c.JSON(http.StatusOK, recipes[i])
-			return
-		}
-	}*/
-
-	c.JSON(http.StatusNotFound, gin.H{"error": "Recipe not found"})
-}
-
-var ctx context.Context
-var err error
-var client *mongo.Client
+var recipesHandler *handlers.RecipesHandler
 
 func init() {
-	/*
-		recipes = make([]Recipe, 0)
-		file, _ := ioutil.ReadFile("recipes.json")
-		_ = json.Unmarshal([]byte(file), &recipes)
-	*/
-	ctx = context.Background()
-	client, err = mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -275,28 +24,16 @@ func init() {
 	}
 
 	log.Println("Connected to MongoDB!")
-	/*
-		var listOfRecipes []interface{}
-		for _, recipe := range recipes {
-			listOfRecipes = append(listOfRecipes, recipe)
-		}
-		collection := client.Database(os.Getenv("MONGO_DATABASE")).Collection("recipes")
-
-		insertManyResult, err := collection.InsertMany(ctx, listOfRecipes)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println("Inserted recipes", len(insertManyResult.InsertedIDs))
-	*/
+	collection := client.Database(os.Getenv("MONGO_DATABASE")).Collection("recipes")
+	recipesHandler = handlers.NewRecipesHandler(ctx, collection)
 }
 
 func main() {
 	router := gin.Default()
-	router.POST("/recipes", NewRecipeHandler)
-	router.GET("/recipes", ListRecipesHandler)
-	router.PUT("/recipes/:id", UpdateRecipeHandler)
-	router.DELETE("/recipes/:id", DeleteRecipeHandler)
-	router.GET("/recipes/:id", GetRecipeHandler)
-	router.GET("/recipes/search", SearchRecipesHandler)
+	router.POST("/recipes", recipesHandler.NewRecipeHandler)
+	router.GET("/recipes", recipesHandler.ListRecipesHandler)
+	router.PUT("/recipes/:id", recipesHandler.UpdateRecipeHandler)
+	router.DELETE("/recipes/:id", recipesHandler.DeleteRecipeHandler)
+	router.GET("/recipes/:id", recipesHandler.GetOneRecipeHandler)
 	router.Run()
 }
